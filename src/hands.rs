5ce27c996759c1f3e4b962daa6d7dc5d0556fe56
Rubén Durá Tarí <rubendura@gmail.com>
2018-02-21 01:45:13 +0000

    Add Secansa struct and allow sorting it

--- src/hands.rs
+++ src/hands.rs
@@ -18,8 +18,8 @@ impl Flor {
             .all_equal()
     }
 
-    fn from_cards(cards: [Card; 3], marker: Card) -> Option<Flor> {
-        if Flor::is_flor(cards, marker) {
+    fn from_cards(cards: [Card; 3], marker: Card) -> Option<Self> {
+        if Self::is_flor(cards, marker) {
             Some(Flor {
                 cards: cards,
                 marker: marker,
@@ -52,25 +52,87 @@ impl Flor {
 }
 
 impl Ord for Flor {
-    fn cmp(&self, other: &Flor) -> Ordering {
+    fn cmp(&self, other: &Self) -> Ordering {
         self.value().cmp(&other.value())
     }
 }
 
 impl PartialOrd for Flor {
-    fn partial_cmp(&self, other: &Flor) -> Option<Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-fn is_secansa(cards: &[&Card; 3]) -> bool {
-    let values = cards.iter().map(|card| card.value).sorted();
-    let next_values = values.iter().filter_map(|value| value.next());
-    values
-        .iter()
-        .skip(1)
-        .zip(next_values)
-        .any(|(value1, value2)| *value1 == value2)
+#[derive(Debug, Eq, PartialEq)]
+struct Secansa {
+    cards: [Card; 3],
+}
+
+impl Secansa {
+    fn is_secansa(cards: [Card; 3]) -> bool {
+        let values = cards.iter().map(|card| card.value).sorted();
+        let next_values = values.iter().filter_map(|value| value.next());
+        values
+            .iter()
+            .skip(1)
+            .zip(next_values)
+            .any(|(value1, value2)| *value1 == value2)
+    }
+
+    fn is_secansa_3_cards(&self) -> bool {
+        let values = self.cards.iter().map(|card| card.value).sorted();
+        let next_values = values.iter().filter_map(|value| value.next());
+        values
+            .iter()
+            .skip(1)
+            .zip(next_values)
+            .all(|(value1, value2)| *value1 == value2)
+    }
+
+    fn highest_card(&self) -> Card {
+        let mut sorted = self.cards.clone();
+        sorted.sort_by_key(|card| card.value);
+
+        let next_values = sorted.iter().filter_map(|card| card.value.next());
+
+        let result = sorted
+            .iter()
+            .skip(1)
+            .zip(next_values)
+            .filter(|&(card, next_value)| card.value == next_value)
+            .map(|(card, _)| *card)
+            .last()
+            .unwrap();
+        result
+    }
+
+    fn from_cards(cards: [Card; 3]) -> Option<Self> {
+        if Self::is_secansa(cards) {
+            Some(Secansa { cards: cards })
+        } else {
+            None
+        }
+    }
+}
+
+impl Ord for Secansa {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.is_secansa_3_cards() && !other.is_secansa_3_cards() {
+            Ordering::Greater
+        } else if !self.is_secansa_3_cards() && other.is_secansa_3_cards() {
+            Ordering::Less
+        } else {
+            let self_max = self.highest_card().value;
+            let other_max = other.highest_card().value;
+            self_max.cmp(&other_max)
+        }
+    }
+}
+
+impl PartialOrd for Secansa {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 fn is_ali(cards: &[&Card; 3]) -> bool {
@@ -369,7 +431,7 @@ mod tests {
     }
 
     #[test]
-    fn flor_sorting() {
+    fn flor_ordering() {
         let marker = Card {
             suit: Suit::Oros,
             value: Value::Uno,
@@ -434,106 +496,244 @@ mod tests {
     #[test]
     fn is_secansa_3_true() {
         let hand = [
-            &Card {
+            Card {
                 suit: Suit::Oros,
                 value: Value::Uno,
             },
-            &Card {
+            Card {
                 suit: Suit::Bastos,
                 value: Value::Dos,
             },
-            &Card {
+            Card {
                 suit: Suit::Espadas,
                 value: Value::Tres,
             },
         ];
-        assert!(is_secansa(&hand));
+        assert!(Secansa::is_secansa(hand));
 
         let hand = [
-            &Card {
+            Card {
                 suit: Suit::Copas,
                 value: Value::Sota,
             },
-            &Card {
+            Card {
                 suit: Suit::Bastos,
                 value: Value::Caballo,
             },
-            &Card {
+            Card {
                 suit: Suit::Espadas,
                 value: Value::Rey,
             },
         ];
-        assert!(is_secansa(&hand));
+        assert!(Secansa::is_secansa(hand));
     }
 
     #[test]
     fn is_secansa_2_true() {
         let hand = [
-            &Card {
+            Card {
                 suit: Suit::Oros,
                 value: Value::Uno,
             },
-            &Card {
+            Card {
                 suit: Suit::Bastos,
                 value: Value::Dos,
             },
-            &Card {
+            Card {
                 suit: Suit::Espadas,
                 value: Value::Cuatro,
             },
         ];
-        assert!(is_secansa(&hand));
+        assert!(Secansa::is_secansa(hand));
 
         let hand = [
-            &Card {
+            Card {
                 suit: Suit::Copas,
                 value: Value::Siete,
             },
-            &Card {
+            Card {
                 suit: Suit::Bastos,
                 value: Value::Caballo,
             },
-            &Card {
+            Card {
                 suit: Suit::Espadas,
                 value: Value::Rey,
             },
         ];
-        assert!(is_secansa(&hand));
+        assert!(Secansa::is_secansa(hand));
     }
 
     #[test]
     fn is_secansa_false() {
         let hand = [
-            &Card {
+            Card {
                 suit: Suit::Oros,
                 value: Value::Uno,
             },
-            &Card {
+            Card {
                 suit: Suit::Bastos,
                 value: Value::Tres,
             },
-            &Card {
+            Card {
                 suit: Suit::Espadas,
                 value: Value::Cinco,
             },
         ];
-        assert!(!is_secansa(&hand));
+        assert!(!Secansa::is_secansa(hand));
 
         let hand = [
-            &Card {
+            Card {
                 suit: Suit::Copas,
                 value: Value::Sota,
             },
-            &Card {
+            Card {
                 suit: Suit::Bastos,
                 value: Value::Sota,
             },
-            &Card {
+            Card {
                 suit: Suit::Espadas,
                 value: Value::Rey,
             },
         ];
-        assert!(!is_secansa(&hand));
+        assert!(!Secansa::is_secansa(hand));
+    }
+
+    #[test]
+    fn is_secansa_3_cards_true() {
+        let hand = [
+            Card {
+                suit: Suit::Oros,
+                value: Value::Uno,
+            },
+            Card {
+                suit: Suit::Bastos,
+                value: Value::Dos,
+            },
+            Card {
+                suit: Suit::Espadas,
+                value: Value::Tres,
+            },
+        ];
+        assert!(Secansa::from_cards(hand).unwrap().is_secansa_3_cards());
+    }
+
+    #[test]
+    fn is_secansa_3_cards_false() {
+        let hand = [
+            Card {
+                suit: Suit::Oros,
+                value: Value::Uno,
+            },
+            Card {
+                suit: Suit::Bastos,
+                value: Value::Dos,
+            },
+            Card {
+                suit: Suit::Espadas,
+                value: Value::Cuatro,
+            },
+        ];
+        assert!(!Secansa::from_cards(hand).unwrap().is_secansa_3_cards());
+    }
+
+    #[test]
+    fn secansa_ordering() {
+        let secansa_3_cards = Secansa {
+            cards: [
+                Card {
+                    suit: Suit::Oros,
+                    value: Value::Uno,
+                },
+                Card {
+                    suit: Suit::Bastos,
+                    value: Value::Dos,
+                },
+                Card {
+                    suit: Suit::Espadas,
+                    value: Value::Tres,
+                },
+            ],
+        };
+        let secansa_real = Secansa {
+            cards: [
+                Card {
+                    suit: Suit::Oros,
+                    value: Value::Uno,
+                },
+                Card {
+                    suit: Suit::Bastos,
+                    value: Value::Dos,
+                },
+                Card {
+                    suit: Suit::Espadas,
+                    value: Value::Tres,
+                },
+            ],
+        };
+        let secansa_2_top = Secansa {
+            cards: [
+                Card {
+                    suit: Suit::Oros,
+                    value: Value::Sota,
+                },
+                Card {
+                    suit: Suit::Bastos,
+                    value: Value::Caballo,
+                },
+                Card {
+                    suit: Suit::Espadas,
+                    value: Value::Tres,
+                },
+            ],
+        };
+        let secansa_2_low = Secansa {
+            cards: [
+                Card {
+                    suit: Suit::Oros,
+                    value: Value::Dos,
+                },
+                Card {
+                    suit: Suit::Bastos,
+                    value: Value::Uno,
+                },
+                Card {
+                    suit: Suit::Espadas,
+                    value: Value::Cinco,
+                },
+            ],
+        };
+        let secansa_2_low_with_high_card = Secansa {
+            cards: [
+                Card {
+                    suit: Suit::Oros,
+                    value: Value::Dos,
+                },
+                Card {
+                    suit: Suit::Bastos,
+                    value: Value::Tres,
+                },
+                Card {
+                    suit: Suit::Espadas,
+                    value: Value::Rey,
+                },
+            ],
+        };
+        let expected = [
+            &secansa_2_low,
+            &secansa_2_low_with_high_card,
+            &secansa_2_top,
+            &secansa_3_cards,
+            &secansa_real,
+        ];
+        let mut result = [
+            &secansa_3_cards,
+            &secansa_2_low_with_high_card,
+            &secansa_2_top,
+            &secansa_real,
+            &secansa_2_low,
+        ];
+        result.sort();
+        assert_eq!(expected, result);
     }
 
     #[test]
