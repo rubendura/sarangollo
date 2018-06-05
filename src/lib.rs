7acff21d08f5cda58f2ae69cdd7333ffbc024686
Rubén Durá Tarí <rubendura@gmail.com>
2018-06-05 01:14:05 +0100

    Add Round get_rey_score

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ mod hands;
 pub mod scoreboard;
 
 use hands::{ali, flor, secansa, Hand};
+use itertools::Itertools;
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Team {
@@ -155,7 +156,7 @@ impl<'a> Round<'a> {
             flor: self.get_flor_score(),
             secansa: self.get_secansa_score(),
             ali: self.get_ali_score(),
-            rey: None,
+            rey: self.get_rey_score(),
             truc: scoreboard::RoundScoreSection(Team::Team1, 0),
         }
     }
@@ -312,6 +313,26 @@ impl<'a> Round<'a> {
         let total = games_value + extra;
 
         Some(scoreboard::RoundScoreSection(winner, total))
+    }
+
+    fn get_rey_score(&self) -> Option<scoreboard::RoundScoreSection> {
+        let winner_team = self.iter_from_hand()
+            .skip_while(|&(_team, seat)| {
+                !seat.face_up_cards
+                    .iter()
+                    .map(|card| card.value)
+                    .any(|value| value == deck::Value::Rey)
+            })
+            .nth(0)?
+            .0;
+
+        let rey_count = self.iter_from_hand()
+            .filter(|&(team, _seat)| team == winner_team)
+            .flat_map(|(_team, seat)| &seat.face_up_cards)
+            .filter(|&card| card.value == deck::Value::Rey)
+            .count() as u8;
+
+        Some(scoreboard::RoundScoreSection(winner_team, rey_count))
     }
 }
 
@@ -1738,5 +1759,249 @@ mod tests {
 
         let expected = Some(scoreboard::RoundScoreSection(Team::Team2, 11));
         assert_eq!(round.get_ali_score(), expected);
+    }
+
+    #[test]
+    fn get_rey_score_no_reyes() {
+        let game = Game::new(vec![Player::new("a")]);
+        let mut round = Round::new(&game, &game.players[0], deck::Deck::default());
+
+        round.seats = vec![Seat {
+            player: &game.players[0],
+            hand: vec![],
+            face_up_cards: vec![
+                deck::Card {
+                    suit: deck::Suit::Oros,
+                    value: deck::Value::Cinco,
+                },
+                deck::Card {
+                    suit: deck::Suit::Copas,
+                    value: deck::Value::Cinco,
+                },
+                deck::Card {
+                    suit: deck::Suit::Copas,
+                    value: deck::Value::Sota,
+                },
+            ],
+        }];
+
+        let result = round.get_rey_score();
+        let expected = None;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn get_rey_score_one_rey() {
+        let game = Game::new(vec![Player::new("a")]);
+        let mut round = Round::new(&game, &game.players[0], deck::Deck::default());
+
+        round.seats = vec![Seat {
+            player: &game.players[0],
+            hand: vec![],
+            face_up_cards: vec![
+                deck::Card {
+                    suit: deck::Suit::Oros,
+                    value: deck::Value::Rey,
+                },
+                deck::Card {
+                    suit: deck::Suit::Copas,
+                    value: deck::Value::Cinco,
+                },
+                deck::Card {
+                    suit: deck::Suit::Copas,
+                    value: deck::Value::Sota,
+                },
+            ],
+        }];
+
+        let result = round.get_rey_score();
+        let expected = Some(scoreboard::RoundScoreSection(Team::Team1, 1));
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn get_rey_score_tres_rey() {
+        let game = Game::new(vec![Player::new("a")]);
+        let mut round = Round::new(&game, &game.players[0], deck::Deck::default());
+
+        round.seats = vec![Seat {
+            player: &game.players[0],
+            hand: vec![],
+            face_up_cards: vec![
+                deck::Card {
+                    suit: deck::Suit::Oros,
+                    value: deck::Value::Rey,
+                },
+                deck::Card {
+                    suit: deck::Suit::Copas,
+                    value: deck::Value::Rey,
+                },
+                deck::Card {
+                    suit: deck::Suit::Copas,
+                    value: deck::Value::Rey,
+                },
+            ],
+        }];
+
+        let result = round.get_rey_score();
+        let expected = Some(scoreboard::RoundScoreSection(Team::Team1, 3));
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn get_rey_score_many_in_team() {
+        let game = Game::new(vec![Player::new("a"), Player::new("b")]);
+        let mut round = Round::new(&game, &game.players[0], deck::Deck::default());
+
+        round.seats = vec![
+            Seat {
+                player: &game.players[0],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Rey,
+                    },
+                ],
+            },
+            Seat {
+                player: &game.players[0],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Dos,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Cinco,
+                    },
+                ],
+            },
+            Seat {
+                player: &game.players[0],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Sota,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Caballo,
+                    },
+                ],
+            },
+            Seat {
+                player: &game.players[0],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Uno,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Cinco,
+                    },
+                ],
+            },
+        ];
+
+        let result = round.get_rey_score();
+        let expected = Some(scoreboard::RoundScoreSection(Team::Team2, 2));
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn get_rey_score_many_in_team_at_hand() {
+        let game = Game::new(vec![Player::new("a"), Player::new("b")]);
+        let mut round = Round::new(&game, &game.players[1], deck::Deck::default());
+
+        round.seats = vec![
+            Seat {
+                player: &game.players[0],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Rey,
+                    },
+                ],
+            },
+            Seat {
+                player: &game.players[1],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Cuatro,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Cinco,
+                    },
+                ],
+            },
+            Seat {
+                player: &game.players[0],
+                hand: vec![],
+                face_up_cards: vec![
+                    deck::Card {
+                        suit: deck::Suit::Oros,
+                        value: deck::Value::Rey,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Sota,
+                    },
+                    deck::Card {
+                        suit: deck::Suit::Copas,
+                        value: deck::Value::Caballo,
+                    },
+                ],
+            },
+        ];
+
+        let result = round.get_rey_score();
+        let expected = Some(scoreboard::RoundScoreSection(Team::Team1, 4));
+
+        assert_eq!(result, expected);
     }
 }
